cf8732cd74ab0b6ea51a23c4f0f6d48c2391d1f7
RomainEtu <romain.ly.etu@gmail.com>
2021-05-05 15:37:11 +0200

    Initial commit

---
+++ src/model/GestionJeu.java
@@ -0,0 +1,211 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Vector;
+
+public class GestionJeu {
+	
+	//Points de l'ordinateur
+	private int _pointsOrdi;
+	
+	//Points du joueur
+	private int _pointsJoueur;
+	
+	//Nombre choisi par le joueur
+	private int _nombreJoueur;
+	
+	//Nombre choisi par l'ordinateur
+	private int _nombreOrdi;
+	
+	//Maximum pour gagner
+	private int _maxPointsGagnants;
+	
+	//générateur de nombre aléatoire
+	private Random _alea; 
+
+
+
+    
+	///////////////////////////////////////////////////////////////////////////////////////////////
+    //Constructeur
+	///////////////////////////////////////////////////////////////////////////////////////////////
+    GestionJeu() {
+    	this._maxPointsGagnants = 10;
+    	this._nombreJoueur = 0;
+    	this._nombreOrdi = 0;
+    	this._pointsJoueur = 0;
+    	this._pointsOrdi = 0;
+    	this._alea = new Random();
+    }
+    
+    
+	///////////////////////////////////////////////////////////////////////////////////////////////
+    //Les accesseurs/modifieurs de données membres
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * @return the _pointsOrdi
+	 */
+	public int get_pointsOrdi() {
+		return _pointsOrdi;
+	}
+
+
+	/**
+	 * @param _pointsOrdi the _pointsOrdi to set
+	 */
+	public void set_pointsOrdi(int _pointsOrdi) {
+		this._pointsOrdi = _pointsOrdi;
+	}
+
+
+	/**
+	 * @return the _pointsJoueur
+	 */
+	public int get_pointsJoueur() {
+		return _pointsJoueur;
+	}
+
+
+	/**
+	 * @param _pointsJoueur the _pointsJoueur to set
+	 */
+	public void set_pointsJoueur(int _pointsJoueur) {
+		this._pointsJoueur = _pointsJoueur;
+	}
+
+
+	/**
+	 * @return the _nombreJoueur
+	 */
+	public int get_nombreJoueur() {
+		return _nombreJoueur;
+	}
+
+
+	/**
+	 * @param _nombreJoueur the _nombreJoueur to set
+	 */
+	public void set_nombreJoueur(int _nombreJoueur) {
+		this._nombreJoueur = _nombreJoueur;
+	}
+
+
+	/**
+	 * @return the _nombreOrdi
+	 */
+	public int get_nombreOrdi() {
+		return _nombreOrdi;
+	}
+
+
+	/**
+	 * @param _nombreOrdi the _nombreOrdi to set
+	 */
+	public void set_nombreOrdi(int _nombreOrdi) {
+		this._nombreOrdi = _nombreOrdi;
+	}
+
+
+	/**
+	 * @return the _maxPointsGagnants
+	 */
+	public int get_maxPointsGagnants() {
+		return _maxPointsGagnants;
+	}
+
+
+	/**
+	 * @param _maxPointsGagnants the _maxPointsGagnants to set
+	 */
+	public void set_maxPointsGagnants(int _maxPointsGagnants) {
+		this._maxPointsGagnants = _maxPointsGagnants;
+	}
+
+
+	/**
+	 * @return the _alea
+	 */
+	public Random get_alea() {
+		return _alea;
+	}
+
+
+	/**
+	 * @param _alea the _alea to set
+	 */
+	public void set_alea(Random _alea) {
+		this._alea = _alea;
+	}
+
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+    //Gestion du jeu
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	public void Init() {
+    	this._nombreJoueur = 0;
+    	this._nombreOrdi = 0;
+ 		this._pointsJoueur = 0;
+		this._pointsOrdi = 0;
+    	this._alea = new Random();
+	}
+
+	public boolean Gagnant() {
+		return ((this._pointsJoueur==_maxPointsGagnants) || (this._pointsOrdi==_maxPointsGagnants)); 
+	}
+
+	public boolean JoueurGagne() {
+		return (this._pointsJoueur == this._maxPointsGagnants);
+	}   
+
+	public void ChoixOrdinateur() {
+		this._nombreOrdi = this._alea.nextInt(3);		
+	}
+
+
+	public int CaculerPoints() {
+		//mise à jour des points
+		//retourne -1 si l'ordinateur gagne la manche
+		//          0 si égalité
+		//          1 si le joueur gagne la manche
+		int res=0;
+		switch ((int)(Math.abs((double)this._nombreJoueur - this._nombreOrdi)))
+		{	
+			case 2: if (this._nombreJoueur > this._nombreOrdi)
+					{
+						this._pointsJoueur++;
+						res = 1;
+					}
+					else
+					{
+						this._pointsOrdi++;
+						res = -1;
+					}
+					break;
+			case 1: if (this._nombreJoueur < this._nombreOrdi) 
+					{
+						this._pointsJoueur++;
+						res = 1;
+					}
+					else
+					{						
+						this._pointsOrdi++;
+						res = -1;
+					}
+					break;
+		}		
+		return res;
+				
+	}
+
+}
+
+	
+	
---
+++ src/model/Jeu.java
@@ -0,0 +1,66 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Jeu {
+
+	public static void main(String[] args) throws Exception {
+		boolean fin, quitterJeu;
+		String reponse, texte;
+		
+		//pour gérer le jeu
+		GestionJeu jeu = new GestionJeu();
+		
+		quitterJeu = false;
+		do
+		{	//initialisation des points
+			jeu.Init();
+			
+			fin = false;
+			do
+			{ //saisie du nombre du joueur: 0, 1, 2 ou 3
+				do
+				{  //le joueur donne un nombre
+					System.out.println("Votre nombre: ");
+					BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+					texte = in.readLine();
+					jeu.set_nombreJoueur(Integer.parseInt(texte));
+				}while ((texte.length() != 1) || (jeu.get_nombreJoueur() <0) || (jeu.get_nombreJoueur()>3));
+				if (jeu.get_nombreJoueur() == 3)
+					//le joueur a donné 3: il faut arrêter le jeu
+					fin = true;
+				else
+				{  //choix aléatoire du nombre de l'ordinateur : 0, 1 ou 2
+					jeu.ChoixOrdinateur();
+					System.out.println("nbord = " + jeu.get_nombreOrdi());
+					jeu.CaculerPoints();
+					
+					System.out.println(" ptsj = "+ jeu.get_pointsJoueur() + " et ptsord = " + jeu.get_pointsOrdi());
+				}
+			}
+			while (!jeu.Gagnant() && (!fin));
+
+			//le jeu s'arrête: affichage du résultat
+			if (! fin) 
+				if (jeu.JoueurGagne())
+					System.out.println("Bravo, vous avez atteint 10 points");
+				else
+					System.out.println("L'ordinateur a atteint 10 points");
+			else if (jeu.get_pointsJoueur() < jeu.get_pointsOrdi()) 
+				System.out.println("L'ordinateur a plus de points que vous!");
+			else if (jeu.get_pointsJoueur() > jeu.get_pointsOrdi()) 
+				System.out.println("Bravo, vous avez plus de points que l'ordinateur!");
+			else
+				System.out.println("Egalité!");
+		    
+			//on demande au joueur s'il veut faire une autre partie
+			System.out.println("Voulez-vous rejouer (o pour oui)?");
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			reponse = in.readLine();
+		    quitterJeu = (reponse.compareTo("o") != 0);
+		}
+		while (!quitterJeu);
+	}
+
+}
